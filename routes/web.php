091f2243d4dedaeb6f3def17ba51e7cc297f731c
ghdcksgml1 <ghdcksgml2@naver.com>
2021-11-27 11:41:35 +0900

    모델 생성과 데이터베이스 사용법

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,5 @@ Route::get('/', 'HomeController@index');
 Route::get('/hello','HomeController@hello');
 
 Route::get('/contact','HomeController@contact');
+
+Route::get('/projects','ProjectController@index');
